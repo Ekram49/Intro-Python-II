fb944b14b3448a23d0dc7c805e1f353d5ada830d
ekram49 <ekramullahzaki@gmail.com>
2020-07-08 19:32:41 -0400

    day-1

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,5 @@
+from player import Player
 from room import Room
-
 # Declare all the rooms
 
 room = {
@@ -49,3 +49,97 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+print("Welcome!!!")
+
+name = input("Player name: ")
+
+location = room['outside']
+
+player = Player(name, location)
+
+print(f"Hello {name}, standing at {location}")
+print("press '1' to go north, '2' to go south, '3' to go east, '4' to go west, 'q' to quit")
+print("Have fun!!!")
+
+print(location)
+
+def choose_action():
+    return int(input("[1] north  [2] south   [3] east  [4] west    [9] Quit\n"))
+
+
+action = choose_action()
+
+
+def next_step():
+    print(location)
+    print('where would you like to go next?')
+    global action
+    action = choose_action()
+
+
+def wrong_way():
+    print('that direction is blocked, try again')
+    global action
+    action = choose_action()
+
+
+while not action == 9:
+    if location == room['outside']:
+        if action == 1:
+            location = room['outside'].n_to
+            next_step()
+        else:
+            location == room['outside']
+            print('wrong way')
+            next_step()
+    elif location == room['foyer']:
+
+        if action == 2:
+            location = room['foyer'].s_to
+            next_step()
+        
+        elif action == 3:
+            location = room['foyer'].e_to
+            next_step()
+
+        elif action == 1:
+            location = room['foyer'].n_to
+            next_step()
+        else:
+            location == room['foyer']
+            print('wrong way')
+            next_step()
+
+    elif location == room['overlook']:
+        if action == 2:
+            location = room['overlook'].s_to
+            next_step()
+        else:
+            location == room['overlook']
+            print('wrong way')
+            next_step()
+
+    elif location == room['narrow']:
+        if action == 4:
+            location = room['narrow'].w_to
+            next_step()
+        elif action == 1:
+            location = room['narrow'].n_to
+            next_step()
+        else:
+            location == room['narrow']
+            print('wrong way')
+            next_step()
+
+    elif location == room['treasure']:
+        if action == 2:
+            location = room['treasure'].s_to
+            next_step()
+        else:
+            location == room['treasure']
+            print('wrong way')
+            next_step()
+
+    else:
+        wrong_way()
